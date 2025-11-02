840c29e3630a08bf50d958c27378c5ddbc79a573
Сырвачев Алексей <Alexey.Syrvachev@urfu.me>
2025-11-02 21:18:39 +0500

    Модуляция

---
+++ js/util.js
@@ -0,0 +1,21 @@
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.lenght - 1)];
+
+function createIdGenerator () {
+  let lastGeneratedId = 0;
+  return function () {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+}
+
+export {getRandomInteger, getRandomArrayElement, createIdGenerator};
+
+
